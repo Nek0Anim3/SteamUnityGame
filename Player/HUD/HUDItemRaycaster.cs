2c906d6d2dd5f378f196b9729dbbca675f73bd77
Nek0Anim3 <pogoderopa@gmail.com>
2026-08-04 12:11:38 +0300

    (fix)UI Item Info, now correctly shows nearest to crosshair if items close enough

--- Player/HUD/HUDItemRaycaster.cs
+++ Player/HUD/HUDItemRaycaster.cs
@@ -6,22 +6,35 @@ public class HUDItemRaycaster : MonoBehaviour
     [SerializeField] private Camera playerCam;
     private float _rayDistance = 1.7f;
     [SerializeField] private LayerMask interactableLayer;
-    private RaycastHit[] _hitResults = new RaycastHit[1];
+    private RaycastHit[] _hitResults = new RaycastHit[5];
     
     public event Action<(string, string, Sprite)> OnItemVisible;
     public event Action<Vector3, Camera> OnItemPositionGet;
     public event Action OnItemDisappeared;
     private bool isVisible;
+    private RaycastHit _currentObject;
+    private RaycastHit _nearestObject;
+    
     void LateUpdate()
     {
         
         Ray ray = playerCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
         Debug.DrawRay(ray.origin, ray.direction * _rayDistance, Color.red);
-        if (Physics.SphereCastNonAlloc(ray, 0.18f, _hitResults, _rayDistance, interactableLayer, QueryTriggerInteraction.Collide) > 0)
+        int hitCount = Physics.SphereCastNonAlloc(ray, 0.18f, _hitResults, _rayDistance, interactableLayer, QueryTriggerInteraction.Collide);
+        if (hitCount > 0)
         {
-            if (isVisible) return;
-            ItemScript itemScript = _hitResults[0].collider.GetComponent<ItemScript>();
+            _nearestObject = _hitResults[0];
+            for (int i = 0; i < hitCount; i++)
+            {
+                if (_hitResults[i].distance < _nearestObject.distance)
+                {
+                    _nearestObject = _hitResults[i];
+                }
+            }
+            if (_currentObject.collider != null && _currentObject.collider == _nearestObject.collider) return;
+            ItemScript itemScript = _nearestObject.collider.GetComponent<ItemScript>();
             OnItemVisible?.Invoke(itemScript.GetItemData());
+            _currentObject = _nearestObject;
             /*OnItemPositionGet?.Invoke(itemScript.GetUIAnchor().position, playerCam);*/
             isVisible = true;
         }
