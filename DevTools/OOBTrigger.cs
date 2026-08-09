eab81cc25515e28b183a0eae93aef1cbd64faa03
Nek0Anim3 <pogoderopa@gmail.com>
2026-08-09 13:17:21 +0300

    OOB Trigger Script

---
+++ DevTools/OOBTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Player;
+using UnityEngine;
+
+namespace DevTools
+{
+    public class OOBTrigger : MonoBehaviour
+    { 
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("[OOB TRIGGER] Player collided!!");
+                var script = collision.gameObject.GetComponent<PlayerOOBTeleport>();
+                script.RespawnPlayer();
+            }
+        }
+    }
+}
