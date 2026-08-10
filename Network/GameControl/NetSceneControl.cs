392cb52e13b807715d1095b026ea116d1bc48c1e
Nek0Anim3 <pogoderopa@gmail.com>
2026-08-10 11:37:53 +0300

    refactor(NetSceneControl) Scene load logic to FishNet

--- Network/GameControl/NetSceneControl.cs
+++ Network/GameControl/NetSceneControl.cs
@@ -21,60 +21,21 @@ public class NetSceneControl : NetworkBehaviour
 
     public override void OnStartServer()
     {
+        base.OnStartServer();
         if (IsServerInitialized)
         {
-            _root.NetworkManager.SceneManager.OnLoadEnd += OnSceneLoadComplete;
+            
             _root.UI.OnStartRequested += ChangeScene;
         }
     }
-    public override void OnStopServer()
-    {
-        if (IsServerInitialized && _root.NetworkManager != null && _root.NetworkManager.SceneManager != null)
-        {
-            _root.NetworkManager.SceneManager.OnLoadEnd -= OnSceneLoadComplete;
-        }
-    }
-    
+
     public void ChangeScene(string sceneName)
     {
-        if (!IsServer) return;
+        if (!IsServerInitialized) return;
         SceneLoadData sceneLoadData = new SceneLoadData(sceneName);
+        sceneLoadData.ReplaceScenes = ReplaceOption.All; // Otherwise it adds scene on top of another
         _root.NetworkManager.SceneManager.LoadGlobalScenes(sceneLoadData);
     }
 
-    private void OnSceneLoadComplete(SceneLoadEndEventArgs args)
-    {
-        //==============================
-        // DEPRECATED
-        //==================================
-        // FISHNET WORKS DIFFERENT
-        // OLD SPAWN LOGIC FOR PLAYERS
-        //=================================
-        
-        /*GameObject[] spawns =  GameObject.FindGameObjectsWithTag("Spawnpoint");
-        
-        int spawnIndex = 0;
-        Debug.Log($"[SceneLoad] Completed: {clientsCompleted.Count}, Timeout: {clientsTimeout.Count}");
-        foreach (var id in clientsTimeout)
-            Debug.LogWarning($"[SceneLoad] Client {id} TIMED OUT");
-        foreach (ulong clientId in clientsCompleted)
-        {
-            
-            Vector3 spawnPosition = Vector3.zero;
-            Quaternion spawnRotation = Quaternion.identity;
 
-            if (spawns.Length > 0)
-            {
-                Transform targetPoint = spawns[spawnIndex % spawns.Length].transform;
-                spawnPosition = targetPoint.position;
-                spawnRotation = targetPoint.rotation;
-                spawnIndex++;
-            }
-            
-            GameObject playerInstance = Instantiate(playerPrefab, spawnPosition, spawnRotation);
-            NetworkObject netObj = playerInstance.GetComponent<NetworkObject>();
-            netObj.SpawnAsPlayerObject(clientId, true); 
-        }*/
-        
-    }
 }
